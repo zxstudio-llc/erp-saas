5cac03b8b1183b3d817cf43cea2fdf9dcd279f21
xiaotianqi <itsupport@zx-studio.com>
2026-01-26 13:20:49 -0500

    Set up a fresh Laravel app

---
+++ resources/js/types/index.ts
@@ -0,0 +1,12 @@
+export type * from './auth';
+export type * from './navigation';
+export type * from './ui';
+
+import type { Auth } from './auth';
+
+export type SharedData = {
+    name: string;
+    auth: Auth;
+    sidebarOpen: boolean;
+    [key: string]: unknown;
+};
